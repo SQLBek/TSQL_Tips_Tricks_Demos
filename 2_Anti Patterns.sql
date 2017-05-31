880ee93924d9c3a80160e7336c460759460e25d3
SQLBek <SQLBek@gmail.com>
2017-05-30 20:32:12 -0500

    Demo Scripts - 20170530

---
+++ 2_Anti Patterns.sql
@@ -0,0 +1,392 @@
+/*-------------------------------------------------------------------
+-- 2 - T-SQL Anti-Patterns
+-- 
+-- Summary: When I see these in code, they make me pause and go
+-- hmmm...
+--
+-- Written By: Andy Yun
+-------------------------------------------------------------------*/
+USE AutoDealershipDemo;
+GO
+
+
+
+
+-----
+-- Example #1 - Sort it my way!
+-- SETUP
+IF EXISTS(SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.SalesSummary'))
+	DROP TABLE dbo.SalesSummary;
+
+CREATE TABLE dbo.SalesSummary (
+	VIN CHAR(17),
+	InvoicePrice MONEY,
+	SellPrice MONEY,
+	TransactionDate DATETIME
+);
+CREATE CLUSTERED INDEX CK_SalesSummary_VIN ON dbo.SalesSummary (VIN);
+-- STOP
+
+
+
+
+-- Turn on Actual Execution Plan
+INSERT INTO dbo.SalesSummary (
+	VIN,
+	InvoicePrice,
+	SellPrice,
+	TransactionDate
+)
+SELECT 
+	Inventory.VIN,
+	Inventory.InvoicePrice,
+	SalesHistory.SellPrice,
+	SalesHistory.TransactionDate
+FROM dbo.Inventory
+INNER JOIN dbo.SalesHistory
+	ON SalesHistory.InventoryID = Inventory.InventoryID
+ORDER BY SalesHistory.TransactionDate DESC
+OPTION(MAXDOP 1);
+GO
+TRUNCATE TABLE SalesSummary
+GO
+
+INSERT INTO dbo.SalesSummary (
+	VIN,
+	InvoicePrice,
+	SellPrice,
+	TransactionDate
+)
+SELECT 
+	Inventory.VIN,
+	Inventory.InvoicePrice,
+	SalesHistory.SellPrice,
+	SalesHistory.TransactionDate
+FROM dbo.Inventory
+INNER JOIN dbo.SalesHistory
+	ON SalesHistory.InventoryID = Inventory.InventoryID
+OPTION(MAXDOP 1);
+GO
+TRUNCATE TABLE SalesSummary
+GO
+-- STOP
+
+
+
+
+
+
+
+
+-- How about a heap?
+-- SETUP
+IF EXISTS(SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.SalesSummaryHeap'))
+	DROP TABLE dbo.SalesSummaryHeap;
+
+CREATE TABLE dbo.SalesSummaryHeap (
+	VIN CHAR(17),
+	InvoicePrice MONEY,
+	SellPrice MONEY,
+	TransactionDate DATETIME
+);
+-- STOP
+
+
+
+
+-- Repeat INSERTs
+INSERT INTO dbo.SalesSummaryHeap (
+	VIN,
+	InvoicePrice,
+	SellPrice,
+	TransactionDate
+)
+SELECT 
+	Inventory.VIN,
+	Inventory.InvoicePrice,
+	SalesHistory.SellPrice,
+	SalesHistory.TransactionDate
+FROM dbo.Inventory
+INNER JOIN dbo.SalesHistory
+	ON SalesHistory.InventoryID = Inventory.InventoryID
+ORDER BY SalesHistory.TransactionDate DESC
+OPTION(MAXDOP 1);
+GO
+TRUNCATE TABLE SalesSummaryHeap
+GO
+
+INSERT INTO dbo.SalesSummaryHeap (
+	VIN,
+	InvoicePrice,
+	SellPrice,
+	TransactionDate
+)
+SELECT 
+	Inventory.VIN,
+	Inventory.InvoicePrice,
+	SalesHistory.SellPrice,
+	SalesHistory.TransactionDate
+FROM dbo.Inventory
+INNER JOIN dbo.SalesHistory
+	ON SalesHistory.InventoryID = Inventory.InventoryID
+OPTION(MAXDOP 1);
+TRUNCATE TABLE SalesSummaryHeap
+GO
+-- STOP
+
+
+-- Turn off Actual Execution Plan
+
+
+
+
+
+
+
+
+-----
+-- Example #2 - Sort and sort again!
+SELECT 
+	Inventory.VIN,
+	Inventory.InvoicePrice,
+	SalesHistory.SellPrice,
+	SalesHistory.TransactionDate
+FROM dbo.Inventory
+INNER JOIN dbo.SalesHistory
+	ON SalesHistory.InventoryID = Inventory.InventoryID
+ORDER BY Inventory.VIN;
+GO
+
+
+
+
+-- What's the difference?
+/*
+SET STATISTICS TIME ON
+*/
+
+-- Run with & without ORDER BY
+SELECT 
+	Inventory.VIN,
+	Inventory.InvoicePrice,
+	SalesHistory.SellPrice,
+	SalesHistory.TransactionDate
+FROM dbo.Inventory
+INNER JOIN dbo.SalesHistory
+	ON SalesHistory.InventoryID = Inventory.InventoryID;
+GO
+
+
+-- Where's this data going?
+
+
+
+
+
+
+
+
+-----
+-- Example #3 - Where'd my day go?
+SELECT COUNT(SalesHistoryID)
+FROM dbo.SalesHistory
+WHERE TransactionDate 
+	BETWEEN '2014-12-01' 
+		AND '2014-12-31';
+
+
+-- Give me all Sales records from December 2014
+
+
+
+
+
+
+
+
+-- But this is WRONG!  Why?
+-- Look at SalesHistory.TransactionDate datatype
+
+
+
+
+-- As DATETIME, what are the real values?
+SELECT 
+	CAST('2014-12-01' AS DATETIME),
+	CAST('2014-12-31' AS DATETIME);
+
+
+
+
+
+
+
+
+-- Query Extrapolated
+SELECT COUNT(SalesHistoryID)
+FROM dbo.SalesHistory
+WHERE TransactionDate 
+	BETWEEN '2014-12-01 00:00:00.000' 
+		AND '2014-12-31 00:00:00.000'
+
+
+
+
+
+
+
+
+-- What am I missing?!
+SELECT COUNT(SalesHistoryID)
+FROM dbo.SalesHistory
+WHERE TransactionDate 
+	BETWEEN '2014-12-31 00:00:00.000' 
+		AND '2014-12-31 23:59:59.997'
+
+
+
+
+
+
+
+
+-- One correct way
+SELECT COUNT(SalesHistoryID)
+FROM SalesHistory
+WHERE (
+	TransactionDate    >= '2014-12-01 00:00:000' 
+	AND TransactionDate < '2015-01-01 00:00:000' 
+);
+/*
+WHERE TransactionDate 
+	BETWEEN '2014-12-01 00:00:00.000' 
+		AND '2014-12-31 23:59:59.997'
+*/
+
+
+
+
+
+
+
+
+-----
+-- Example #4 - How long? Who cares?
+DECLARE 
+	@myValue VARCHAR = 'Andy',
+	@aLongerString VARCHAR = 'This is a much longer string which has of 57 characters.';
+
+SELECT 
+	@MyValue, 
+	@aLongerString;
+
+
+
+
+
+
+
+
+-- CAST & CONVERT
+SELECT 
+	CAST('This is a much longer string which has of 57 characters.' AS VARCHAR) AS Casted,
+	LEN(CAST('This is a much longer string which has of 57 characters.' AS VARCHAR)) AS CastedLength,
+	CONVERT(VARCHAR, 'This is a much longer string which has of 57 characters.') AS Converted,
+	LEN(CONVERT(VARCHAR, 'This is a much longer string which has of 57 characters.')) AS ConvertedLength;
+
+
+
+
+
+
+
+
+-- What about a table?
+IF OBJECT_ID('tempdb.dbo.#tmpDeclarationsMatter','U') IS NOT NULL
+	DROP TABLE #tmpDeclarationsMatter;
+
+CREATE TABLE #tmpDeclarationsMatter (
+	ColOne VARCHAR,
+	ColTwo VARCHAR DEFAULT('A Longer string')
+);
+
+
+
+
+-- Insert a value?
+INSERT INTO #tmpDeclarationsMatter (ColOne) VALUES ('Sadie');
+
+
+
+
+-- Insert with DEFAULT?
+INSERT INTO #tmpDeclarationsMatter (ColTwo) VALUES (DEFAULT);
+
+
+
+
+SELECT * 
+FROM #tmpDeclarationsMatter;
+
+-- sp_help
+
+
+
+
+
+
+
+
+-----
+-- Example #5 - SELECT that SELECT!
+SET STATISTICS TIME ON;
+
+SELECT 
+	Inventory.InventoryID,
+	Inventory.VIN,
+	Inventory.InvoicePrice,
+	ISNULL(
+		(
+			SELECT SalesHistory.SellPrice - Inventory.InvoicePrice
+			FROM dbo.SalesHistory
+			WHERE Inventory.InventoryID = SalesHistory.InventoryID
+		)
+	, 0) AS NetProfit
+FROM dbo.Inventory;
+
+
+--
+SET STATISTICS IO ON;
+
+
+
+
+
+
+
+
+SELECT 
+	Inventory.InventoryID,
+	Inventory.VIN,
+	Inventory.InvoicePrice,
+	SalesHistory.SellPrice - Inventory.InvoicePrice AS NetProfit
+FROM dbo.Inventory
+INNER JOIN dbo.SalesHistory
+	ON Inventory.InventoryID = SalesHistory.InventoryID;
+
+
+--
+SET STATISTICS TIME OFF;
+SET STATISTICS IO OFF;
+
+
+
+
+
+
+
+
+---------------------------------------------
+---------------------------------------------
+---------------------------------------------
+---------------------------------------------
---
+++ 3_Common Challenges.sql
@@ -0,0 +1,459 @@
+/*-------------------------------------------------------------------
+-- 3 - Clever Solutions to Common but Not Easily Coded Challenges
+-- 
+-- Summary: Cool script examples to keep in your toolkit for
+-- that rainy day when you need it.
+-- 
+-- Written By: Andy Yun
+-------------------------------------------------------------------*/
+USE AutoDealershipDemo;
+GO
+
+
+
+
+-----
+-- Example #1 - Fun with OUTPUT & inserted
+-- SETUP
+IF OBJECT_ID('tempdb.dbo.#tmpDestination_One') IS NOT NULL
+	DROP TABLE #tmpDestination_One;
+
+CREATE TABLE #tmpDestination_One (
+	RecID INT IDENTITY(1, 3),
+	VIN CHAR(17)
+);
+
+IF OBJECT_ID('tempdb.dbo.#tmpDestination_Two') IS NOT NULL
+	DROP TABLE #tmpDestination_Two;
+
+CREATE TABLE #tmpDestination_Two (
+	RecID INT IDENTITY(1, 1),
+	InsertedRecID INT,
+	VIN CHAR(17)
+);
+GO
+-- STOP
+
+
+
+
+INSERT INTO #tmpDestination_One(VIN)
+OUTPUT inserted.RecID, inserted.VIN			-- What's this?!
+INTO #tmpDestination_Two(InsertedRecID , VIN)
+SELECT TOP 1000 VIN
+FROM dbo.Inventory;
+
+
+
+
+SELECT RecID, VIN
+FROM #tmpDestination_One;
+
+SELECT RecID, InsertedRecID, VIN
+FROM #tmpDestination_Two;
+
+
+-- What is OUTPUT & inserted?
+-- Be clever!
+-- Reference URL: https://docs.microsoft.com/en-us/sql/t-sql/queries/output-clause-transact-sql
+
+
+
+
+
+
+
+
+-----
+-- Example #2 - Horizontal Aggregations?  MIN & MAX across columns?
+SELECT Inventory.InventoryID,
+	Inventory.InvoicePrice,
+	Inventory.MSRP,
+	SalesHistory.SellPrice
+FROM dbo.Inventory
+INNER JOIN dbo.SalesHistory
+	ON Inventory.InventoryID = SalesHistory.InventoryID
+ORDER BY Inventory.InventoryID
+
+
+-- Fun with VALUES()
+
+
+
+
+
+
+
+
+SELECT Inventory.InventoryID,
+	Inventory.InvoicePrice,
+	Inventory.MSRP,
+	SalesHistory.SellPrice,
+	(
+		SELECT MIN(PriceArray.Price)
+		FROM (
+			VALUES 
+				(Inventory.InvoicePrice),
+				(Inventory.MSRP),
+				(SalesHistory.SellPrice)
+			) AS PriceArray(Price)
+	) AS MinPrice
+FROM dbo.Inventory
+INNER JOIN dbo.SalesHistory
+	ON Inventory.InventoryID = SalesHistory.InventoryID
+ORDER BY Inventory.InventoryID;
+
+
+-- Didn't you say don't RBAR?
+SET STATISTICS IO ON;
+
+
+
+
+-- What happened?  Show Actual Execution Plan
+
+
+-- Turn off Actual Execution Plan
+SET STATISTICS IO OFF;
+
+
+
+
+
+
+
+
+-----
+-- Example #3 - Comma Separated Tricks
+SELECT TOP 5 VIN
+FROM dbo.Inventory
+
+
+
+
+
+
+
+
+-- STUFF() & XML to the rescue!
+
+
+
+
+-- FOR XML PATH('')
+SELECT TOP 5 VIN
+FROM dbo.Inventory
+FOR XML PATH('');
+
+
+
+
+-- Add our delimiter
+SELECT TOP 5 ',' + VIN
+FROM dbo.Inventory
+FOR XML PATH('');
+
+
+
+
+-- We must still chop off the first extra comma!  
+
+
+
+
+
+
+
+
+-- Could use SUBSTRING?
+SELECT 
+	SUBSTRING(
+		(
+			SELECT TOP 5 ',' + VIN
+			FROM dbo.Inventory
+			FOR XML PATH('')
+		), 2, 8000	
+	) AS Column2CSVString;
+
+
+-- Problem: How do I know what the actual length of my string is?
+
+
+
+
+
+
+
+
+-- Use STUFF!
+--
+-- STUFF(character_expression, start, length, replaceWith_expression)  
+-- Deletes a specified length of characters in the first string,
+-- at the start position, 
+-- then inserts the second string into the first string,
+-- at the start position.
+SELECT 
+	'ABCDEFGHIJK' AS OriginalString,
+	STUFF(
+		'ABCDEFGHIJK',	-- starting string
+		3,				-- start position
+		5,				-- number of characters to replace
+		'-12345-'		-- replacement string
+	) AS StuffedString;
+
+
+
+
+-- Stuff is a poor man's LEFT TRUNCATE!
+SELECT
+	',Sadie,Jess' AS OriginalString,
+	STUFF(
+		',Sadie,Jess',
+		1,				-- start position
+		1,				-- number of char to replace
+		''				-- replace with nothing!
+	) AS LeftTruncatedString;
+
+
+
+
+
+
+
+-- FINAL
+SELECT 
+	STUFF(
+		(
+			SELECT TOP 5 ',' + VIN
+			FROM dbo.Inventory
+			FOR XML PATH('')
+		), 1, 1, ''
+	) AS Column2CSVString;
+
+
+
+
+
+
+
+
+-- Grouped?!
+IF OBJECT_ID('tempdb.dbo.#tmpVehicleList','U') IS NOT NULL
+	DROP TABLE #tmpVehicleList;
+
+CREATE TABLE #tmpVehicleList (
+	RecID INT IDENTITY(1, 1) PRIMARY KEY CLUSTERED,
+	ModelName VARCHAR(50),
+	ColorName VARCHAR(50)
+);
+
+INSERT INTO #tmpVehicleList (ModelName, ColorName) 
+SELECT Model.ModelName, Color.ColorName
+FROM Vehicle.BaseModel
+INNER JOIN Vehicle.Model
+	ON BaseModel.ModelID = Model.ModelID
+INNER JOIN Vehicle.Color
+	ON BaseModel.ColorID = Color.ColorID
+WHERE BaseModelID % 5 = 0
+	AND ModelName IN ('4Runner', 'Camry', 'Highlander')
+UNION 
+SELECT Model.ModelName, Color.ColorName
+FROM Vehicle.BaseModel
+INNER JOIN Vehicle.Model
+	ON BaseModel.ModelID = Model.ModelID
+INNER JOIN Vehicle.Color
+	ON BaseModel.ColorID = Color.ColorID
+WHERE BaseModelID % 7 = 1
+ORDER BY Model.ModelName, Color.ColorName
+
+SELECT 
+	RecID,
+	ModelName, 
+	ColorName
+FROM #tmpVehicleList;
+
+
+
+
+
+
+
+
+-- Apply FOR XML PATH('') again!
+SELECT 
+	ModelList.ModelName,
+	STUFF(
+		(
+			SELECT 
+				',' + ColorCSVList.ColorName 
+			FROM #tmpVehicleList AS ColorCSVList
+			WHERE ModelList.ModelName = ColorCSVList.ModelName	-- Like a SELF JOIN
+			FOR XML PATH('')
+		), 1, 1, ''
+	) AS Column2CSVString  
+FROM #tmpVehicleList AS ModelList
+GROUP BY ModelList.ModelName
+
+
+
+
+
+
+
+
+-----
+-- Example #4 - Fun with String Functions!
+IF OBJECT_ID('tempdb.dbo.#tmpFilePath') IS NOT NULL
+	DROP TABLE #tmpFilePath;
+
+CREATE TABLE #tmpFilePath (
+	RecID INT IDENTITY(1, 1) PRIMARY KEY CLUSTERED,
+	FilePath NVARCHAR(256)
+);
+INSERT INTO #tmpFilePath 
+VALUES (
+	'C:\My Documents\FolderName\AnotherFolder\MyTextFile.txt'
+);
+
+
+--
+SELECT FilePath
+FROM #tmpFilePath;
+
+-- Give me the Filename only!
+-- MyTextFile.txt
+
+
+
+
+
+
+
+
+-- CHARINDEX()?
+SELECT FilePath,
+	CHARINDEX('\', FilePath) AS LeftCharIndex
+FROM #tmpFilePath;
+
+
+
+
+
+
+
+
+-- Drive in REVERSE!
+SELECT FilePath,
+	CHARINDEX('\', FilePath) AS LeftCharIndex,
+	REVERSE(FilePath) AS FilePathReversed
+FROM #tmpFilePath;
+
+
+
+
+
+
+
+
+-- CHARINDEX on the right side
+SELECT FilePath,
+	CHARINDEX('\', FilePath) AS LeftCharIndex,
+	REVERSE(FilePath) AS FilePathReversed,
+	CHARINDEX('\', 
+		REVERSE(FilePath)
+	) AS RightCharIndex
+FROM #tmpFilePath;
+
+
+
+
+
+
+
+
+-- Like an array
+--
+-- C:\My Documents\FolderName\AnotherFolder\MyTextFile.txt
+--   *                                     *
+-- 1234567890123456789012345678901234567890123456789012345
+--
+-- txt.eliFtxeTyM\redloFrehtonA\emaNredloF\stnemucoD yM\:C
+--               *
+
+
+
+
+
+
+
+
+-- Do some math to get the right-most position
+SELECT FilePath,
+	CHARINDEX('\', FilePath) AS LeftCharIndex,
+	REVERSE(FilePath) AS FilePathReversed,
+	CHARINDEX('\', 
+		REVERSE(FilePath)
+	) AS RightCharIndex,
+	LEN(FilePath) AS FilePathLength,
+	LEN(FilePath) - CHARINDEX('\', REVERSE(FilePath)) AS RightMostDelimiter
+FROM #tmpFilePath;
+
+
+
+
+
+
+
+
+-- SUBSTRING()
+SELECT FilePath,
+	CHARINDEX('\', FilePath) AS LeftCharIndex,
+	REVERSE(FilePath) AS FilePathReversed,
+	CHARINDEX('\', 
+		REVERSE(FilePath)
+	) AS RightCharIndex,
+	LEN(FilePath) AS FilePathLength,
+	LEN(FilePath) - CHARINDEX('\', REVERSE(FilePath)) + 1 AS RightMostDelimiter,
+	SUBSTRING(
+		-- Original String
+		FilePath,
+		-- RightMostDelimiter plus another 1, as starting point
+		LEN(FilePath) - CHARINDEX('\', REVERSE(FilePath)) + 2,
+		-- Number of characters to the right, to take. Can just use LEN() here.
+		LEN(FilePath)
+	) AS MyFileName
+FROM #tmpFilePath;
+
+
+-- Note my methodology
+
+
+
+
+
+
+
+
+-- Yes, there's at least two better ways to do this too!
+SELECT 
+	-- Take SUBSTRING of REVERSE'd string, then re-REVERSE
+	REVERSE(
+		SUBSTRING(
+			REVERSE(FilePath),
+			1,
+			CHARINDEX('\', REVERSE(FilePath)) - 1
+		)
+	) AS Thanks_FrankRess,
+	-- Use RIGHT()!
+	RIGHT(
+		FilePath, 
+		CHARINDEX('\', REVERSE (FilePath)) - 1
+	) AS Thanks_JeffSmalley
+FROM #tmpFilePath;
+
+
+
+---------------------------------------------
+---------------------------------------------
+---------------------------------------------
+---------------------------------------------
